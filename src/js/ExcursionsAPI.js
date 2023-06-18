ea35d5eabf54b3f2e3f47c621de3415333c174ff
Ireneusz Cislo <irekc86@o2.pl>
2023-06-18 05:33:13 +0200

    display none for ulEl prototype and new ulEl in html element

--- src/js/ExcursionsAPI.js
+++ src/js/ExcursionsAPI.js
@@ -9,11 +9,13 @@ loadExcursions() {
     this._fetch(this.urlExcursions)
         .then(data => this.insertExcursions(data))
         .catch(err => console.error(err))
-}
-
-insertExcursions(excursionsArr) {
-    const ulEl = document.querySelector('.panel__excursions');
-    // ulEl.innerHTML = '';
+    }
+    
+    insertExcursions(excursionsArr) {
+        const ulEl = document.querySelector('.panel__excursions');
+        const liElPrototype = document.querySelector('.excursions__item--prototype');
+        ulEl.innerHTML = '';
+    
     excursionsArr.forEach( item => {
         const liEl = this._createLiEl(item);
         ulEl.appendChild(liEl)
@@ -21,8 +23,26 @@ insertExcursions(excursionsArr) {
 
 }   
 
+updateExcursions(arrWithElToEdit) {
+    if(e.target.value === 'edytuj') {
+        e.target.value = 'zapisz';
+        arrWithElToEdit.forEach(item => {
+            item.setAttribute('contenteditable', true)
+        })
+    } else {
+        e.target.value = 'edytuj';
+        arrWithElToEdit.forEach(item => {
+            item.setAttribute('contenteditable', false)
+        })
+    }
+}
+
+removeExcursions() {
+    const ulEl = document.querySelector('.panel__excursions');
+
+};
+
 _createLiEl( item ) {
-    console.log(item)
     const {id, name, description, adultPrice, childPrice} = item;
 
     const liElPrototype = document.querySelector('.excursions__item--prototype');
@@ -34,6 +54,7 @@ _createLiEl( item ) {
     const [adultEl, childEl, submitEL] = formEl.children;
     const [adultPriceEl] = adultEl.children[0].children;
     const [childPriceEl] = childEl.children[0].children;
+    const [editBtn, removeBtn] = submitEL.children;
 
     newLiEl.dataset.id = id;
     titleLi.innerText = name;
@@ -41,10 +62,21 @@ _createLiEl( item ) {
     adultPriceEl.innerText = adultPrice;
     childPriceEl.innerText = childPrice;
 
-    console.dir(adultEl.children[0])    
-    console.log(newLiEl)
+    editBtn.addEventListener('click', function(e) {
+        e.preventDefault()
+        const ElToSetAttribute = [titleLi, descriptionLi, adultPriceEl, childPriceEl]
+        this.updateExcursions(ElToSetAttribute)
+    });
+    removeBtn.addEventListener('click', removeHandler);
+
+    function removeHandler(e) {
+        e.preventDefault();
+        console.log(e.target)
+    }
+
     return newLiEl
 }
+
 
 _fetch(url, options) {
     return fetch(url, options)
